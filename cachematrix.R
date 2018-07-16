f6c66175646dfad7ddeeae89a63ae3d697cfc5dc
nmohan30 <nikithamohan@yahoo.com>
2018-07-16 01:13:40 -0700

    Update cachematrix.R
    first draft of programming exercise 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## Put comments here that give an overall description of what your functions do
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+
+makeCacheMatrix <- function(original.matrix = matrix()) {
+	
+	# this is to make sure we have a valid matrix 
+	if (!is.matrix(original.matrix)) {
+		stop("Please give a valid matrix")
+		}
+		
+		inverted.matrix <- NULL
+		
+		#set value of matrix 
+		set <- function(y = matrix){
+			original <<- y 
+			inverse <<- NULL
+		}
+		
+		#get value of matrix
+		get <- function(y = matrix)
+			
+		#set value of inverse matrix	
+		setInverse <- function(solve) inverse <<- solve 
+		
+		#get value of inverse matrix
+		getInverse <- function() inverse
+		
+		#return list 
+		list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+#returns inverse of matrix
+cacheSolve <- function(original, ...) {
+	inverse <- original$getInverse() # get from cache
+	if(!is.null(inverse))
+		message("Getting cached inverse matrix")
+		return(inverse)
+		}
+		
+		# now here we use call our functions we made in makeCacheMatrix
+		inverse.matrix <- original$get()
+		inverse <- solve(data, ...)
+		original$setsolve(inverse)
+		inverse
+)
 
-## Write a short comment describing this function
+           
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+                              
+ 	
+
